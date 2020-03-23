e053f37ad9f4d148f6186f6e1afbca51616d0344
root <root@laravel.us-central1-a.c.ancient-edition-248215.internal>
2020-03-23 22:32:03 +0000

    6. exponer datos a traves de http get

--- server.php
+++ server.php
@@ -1,21 +1,48 @@
 <?php
-
+//definimos los recursos disponibles
 $allowedResourcesTypes=[
     'books',
     'authors',
     'genres',
 ];
 
-//vamos a hacer la validacion si el tipode recuso que se esta solicitando esta dentro de los recursos disponibles
+//vamos a hacer la validacion si el tipo de recuso que se esta solicitando esta dentro de los recursos disponibles
 $resourcesType= $_GET['resource_type'];
 //con la funcion in_array verificamos que un elemento pertence a un arreglo
 if(!in_array($resourcesType,$allowedResourcesTypes)){
     die;
 }
+
+//Definio los recuros, esto simulando una base de datos, vamos a hacerlo con un arreglo
+$books= [
+    1 => [
+        'titulo' => 'Lo que el viento se llevo',
+        'id_autor' => 2,
+        'id_genero' => 2,
+    ],
+    2 => [
+        'titulo' => 'La Iliada',
+        'id_autor' => 1,
+        'id_genero' => 1,
+    ],
+    3 => [
+        'titulo' => 'La Odisea',
+        'id_autor' => 1,
+        'id_genero' => 1,
+    ],
+];
+
+//vamos a indicarle al usuario que le vamos a enviar un json
+
+header('Content-Type: application/json');
     
 //Me voy a valer de un metodo para saber que tipo de peticion estoy recibien y paso todo a mayuscula. 
+//generamos la respuesta asumiendo que el pedido es correcto. 
     switch( strtoupper($_SERVER['REQUEST_METHOD'])){
+        //vamos con GET a devolver la coleccion de libros
         case 'GET':
+            //lo devolvemos en formato json
+            echo json_encode($books);
         break;
         case 'POST':
         break;
